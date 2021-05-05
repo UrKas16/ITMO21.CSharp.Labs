9f3864d194910a0bc7469b4eca35666d53c7093e
kasyu <kasyu@DESKTOP-C3O5QE8>
2021-05-05 19:58:31 +0300

    change rename

---
+++ Labs03.Using.ExpExc.ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labs03.Using.ExpExc.ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
