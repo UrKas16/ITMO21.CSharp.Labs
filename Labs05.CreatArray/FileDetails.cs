197bcd6d7735462af01997103927f13c180762ca
kasyu <kasyu@DESKTOP-C3O5QE8>
2021-05-10 23:49:14 +0300

    Лаб5. Начало работы

---
+++ Labs05.CreatArray/FileDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labs05.CreatArray
+{
+    class FileDetails
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
