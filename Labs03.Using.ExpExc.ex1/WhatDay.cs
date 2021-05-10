5ebe90c1681cc79aafbc6061967f91f1b9fb319a
kasyu <kasyu@DESKTOP-C3O5QE8>
2021-05-10 17:00:44 +0300

    Лаб3, упр. 1, замена 12 if-else

--- Labs03.Using.ExpExc.ex1/WhatDay.cs
+++ Labs03.Using.ExpExc.ex1/WhatDay.cs
@@ -28,171 +28,18 @@ namespace Labs03.Using.ExpExc.ex1
 
             int monthNum = 0;
 
-            if (dayNum <= 31)
-            { // January
-                goto End;
-            }
-            else
+            foreach (int daysInMonth in DaysInMonths)
             {
-                dayNum -= 31;
-                monthNum++;
+                if (dayNum <= daysInMonth)
+                {
+                    break;
+                }
+                else
+                {
+                    dayNum -= daysInMonth;
+                    monthNum++;
+                }
             }
-
-            if (dayNum <= 28)
-            { // February
-                goto End;
-            }
-            else
-            {
-                dayNum -= 28;
-                monthNum++;
-            }
-
-            if (dayNum <= 28)
-            { // March
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 30)
-            { // April
-                goto End;
-            }
-            else
-            {
-                dayNum -= 30;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)
-            { // May
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 30)
-            { // June
-                goto End;
-            }
-            else
-            {
-                dayNum -= 30;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)
-            { // Jule
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)
-            { // August
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 30)
-            { // September
-                goto End;
-            }
-            else
-            {
-                dayNum -= 30;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)
-            { // October
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 30)
-            { // November
-                goto End;
-            }
-            else
-            {
-                dayNum -= 30;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)
-            { // December
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
-
-        End:
-            //string monthName;
-
-            //switch (monthNum)
-            // {
-            //    case 0:
-            //        monthName = "January";
-            //        break;
-            //    case 1:
-            //        monthName = "February";
-            //        break;
-            //    case 2:
-            //        monthName = "March";
-            //        break;
-            //    case 3:
-            //        monthName = "April";
-            //        break;
-            //    case 4:
-            //        monthName = "May";
-            //        break;
-            //    case 5:
-            //        monthName = "June";
-            //        break;
-            //    case 6:
-            //        monthName = "Jule";
-            //        break;
-            //    case 7:
-            //        monthName = "August";
-            //        break;
-            //    case 8:
-            //        monthName = "September";
-            //        break;
-            //    case 9:
-            //        monthName = "October";
-            //        break;
-            //    case 10:
-            //        monthName = "November";
-            //        break;
-            //    case 11:
-            //        monthName = "December";
-            //        break;
-            //    default:
-            //        monthName = "not done yet";
-            //        break;
-            //}
 
             MonthName temp = (MonthName)monthNum;
             string monthName = temp.ToString();
@@ -200,5 +47,9 @@ namespace Labs03.Using.ExpExc.ex1
             Console.WriteLine("{0}, {1}", dayNum, monthName);
             Console.ReadLine();
         }
+
+        static System.Collections.ICollection DaysInMonths
+            = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
     }
 }
+
