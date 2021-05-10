1f1c475d54d7a4ecea34e6cea9a89db1857512f1
kasyu <kasyu@DESKTOP-C3O5QE8>
2021-05-10 20:09:36 +0300

    Лаб4, Создание и использование методов

---
+++ Labs04.CreatMetods/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs04.CreatMetods
+{
+    class Test
+    {
+        public static void Main()
+        {
+            int x;
+            int y;
+            int greater;
+
+            Console.WriteLine("Ввведите первое число: ");
+            x = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ввведите второе число: ");
+            y = Int32.Parse(Console.ReadLine());
+
+            greater = Utils.Greater(x, y);
+        }
+    }
+}
---
+++ Labs04.CreatMetods/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Labs04.CreatMetods
+{
+    class Utils
+    {
+        public static int Greater(int a, int b) 
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
+
+
