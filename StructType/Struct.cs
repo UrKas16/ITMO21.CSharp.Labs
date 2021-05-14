25aa52cdcb27e9034d137b2811c84ae04471ef4f
Yurii <kasyurvik568@gmail.com>
2021-05-14 14:53:17 +0300

    new project and bankaccunt changes

--- StructType/Struct.cs
+++ StructType/Struct.cs
@@ -25,26 +25,26 @@ namespace StructType
             goldAccount.accBal = (decimal)3200.00;
             goldAccount.accNo = long.Parse(Console.ReadLine());
 
-            //if (goldAccount.accNo == 321)
-            //{
-            //    Console.WriteLine("Account is true");
-            //    Console.WriteLine("*** Account Summary ***");
-            //    Console.WriteLine("Account Number {0}", goldAccount.accNo);
-            //    Console.WriteLine("Account Type {0}", goldAccount.accType);
-            //    Console.WriteLine("Account Balance {0}", goldAccount.accBal);
-            //}
+            if (goldAccount.accNo == 321)
+            {
+                Console.WriteLine("Account is true");
+                Console.WriteLine("*** Account Summary ***");
+                Console.WriteLine("Account Number {0}", goldAccount.accNo);
+                Console.WriteLine("Account Type {0}", goldAccount.accType);
+                Console.WriteLine("Account Balance {0}", goldAccount.accBal);
+            }
 
-            //else
-            //{
-            //    Console.WriteLine("Wrong Account!!! Please try again!!");            
+            else
+            {
+                Console.WriteLine("Wrong Account!!! Please try again!!");
 
-            //}
+            }
 
 
-            Console.WriteLine("*** Account Summary ***");
-            Console.WriteLine("Account Number {0}", goldAccount.accNo);
-            Console.WriteLine("Account Type {0}", goldAccount.accType);
-            Console.WriteLine("Account Balance {0}", goldAccount.accBal);
+            //Console.WriteLine("*** Account Summary ***");
+            //Console.WriteLine("Account Number {0}", goldAccount.accNo);
+            //Console.WriteLine("Account Type {0}", goldAccount.accType);
+            //Console.WriteLine("Account Balance {0}", goldAccount.accBal);
             Console.Read();
         }
     }
