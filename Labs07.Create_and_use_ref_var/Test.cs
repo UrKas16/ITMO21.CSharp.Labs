e5998d5db686f7d0825aa75b7d17c7d8d7978a38
Yurii <kasyurvik568@gmail.com>
2021-05-16 22:50:23 +0300

    Лаб_07, упр_2 - реверс строки

--- Labs07.Create_and_use_ref_var/Test.cs
+++ Labs07.Create_and_use_ref_var/Test.cs
@@ -10,21 +10,27 @@ namespace Labs07.Create_and_use_ref_var
     {
         public static void Main()
         {
-            BankAccount b1 = new BankAccount();
-            b1.Populate(100);
+            //BankAccount b1 = new BankAccount();
+            //b1.Populate(100);
 
-            BankAccount b2 = new BankAccount();
-            b2.Populate(100);
+            //BankAccount b2 = new BankAccount();
+            //b2.Populate(100);
 
-            Console.WriteLine("До пересылки:");
-            Console.WriteLine("{0} {1} {2}", b1.Type(), b1.Number(), b1.Balance());
-            Console.WriteLine("{0} {1} {2}", b2.Type(), b2.Number(), b2.Balance());
+            //Console.WriteLine("До пересылки:");
+            //Console.WriteLine("{0} {1} {2}", b1.Type(), b1.Number(), b1.Balance());
+            //Console.WriteLine("{0} {1} {2}", b2.Type(), b2.Number(), b2.Balance());
 
-            b1.TransferFrom(b2, 10);
+            //b1.TransferFrom(b2, 10);
 
-            Console.WriteLine("После пересылки:");
-            Console.WriteLine("{0} {1} {2}", b1.Type(), b1.Number(), b1.Balance());
-            Console.WriteLine("{0} {1} {2}", b2.Type(), b2.Number(), b2.Balance());
+            //Console.WriteLine("После пересылки:");
+            //Console.WriteLine("{0} {1} {2}", b1.Type(), b1.Number(), b1.Balance());
+            //Console.WriteLine("{0} {1} {2}", b2.Type(), b2.Number(), b2.Balance());
+
+            string message;
+            Console.WriteLine("Введите ваше сообщение: ");
+            message = Console.ReadLine();
+
+            Utils.
 
         }
     }
---
+++ Labs07.Create_and_use_ref_var/Utils.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Labs04.CreatMetods
+{
+    class Utils
+    {
+        public static int Greater(int a, int b) 
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+         }
+
+        public static bool Factorial(int n, out int answer)
+        {
+            int k;
+            int f = 1;
+            bool ok = true;
+
+            if (n < 0)
+            {
+                ok = false;
+            }
+
+            try
+            {
+                checked
+                {
+                    for (k = 2; k <= n; ++k)
+                    {
+                        f = k * f;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                f = 0;
+                ok = false;            
+            }
+
+            answer = f;
+            return ok;                    
+        
+        }
+
+
+        public static void Reverse(ref string s)
+        {
+            string sRev = "";
+
+            while (s.Length != 0)
+            {
+                sRev += s[-1];
+                s.Remove(s[-1]);
+           
+            }
+
+            Console.WriteLine(sRev);
+        
+        }
+    }
+}
+
+
