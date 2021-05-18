e22a6186c2c1cbf75ec603b3450e3e6f94ca6b85
Yurii <kasyurvik568@gmail.com>
2021-05-18 16:52:56 +0300

    ЛАБ_08, УПР_2 - Инициализация данных только для чтения

---
+++ Labs08.CreatObject.exemp.1/BankTransaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class BankTransaction
+{
+	private readonly decimal amount;
+	private readonly DateTime when;
+
+	public BankTransaction(decimal tranAmount)
+	{
+		amount = tranAmount;
+		when = DateTime.Now;
+	}
+
+	public decimal Amount()
+	{
+		return amount;
+	}
+
+	public DateTime When()
+	{
+		return when;
+	}
+}
+
